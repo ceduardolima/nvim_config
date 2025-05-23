91c322d48f74a28066b01b51aaaebe21e306d545
Eduardo <ceduardolima2000@gmail.com>
2025-05-23 17:34:28 -0300

    adicionando o mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,45 @@
+return {
+{
+    "williamboman/mason.nvim",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      local mason_tool_installer = require "mason-tool-installer"
+      mason_tool_installer.setup {
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua", -- lua formatter
+          "isort", -- python formatter
+          "black", -- python formatter
+          "ruff", -- python linter
+          "djlint", -- django linter
+          "eslint_d", -- js linter
+          "gci",
+          "gotests",
+        },
+      }
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = {
+          "lua_ls",
+          "csharp_ls",
+          "elixirls",
+          "arduino_language_server",
+          "clangd",
+          "ts_ls",
+          "sqlls",
+          "gopls",
+          "ast_grep",
+          "jdtls",
+          "pyright",
+        },
+      }
+    end,
+  }
+}
