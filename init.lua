e18919107e2e8f930c08907b0a5a05d0bd05317c
Eduardo <ceduardolima2000@gmail.com>
2025-08-19 15:13:16 -0300

    mudanças no linter e no tema

--- init.lua
+++ init.lua
@@ -1,20 +1,22 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require "core.vim-config"
-require("lazy").setup "plugins"
+require("core.vim-config")
+require("lazy").setup("plugins")
 
-vim.cmd [[colorscheme gruvbox-material]]
+vim.cmd("set background=light")
+
+vim.cmd([[colorscheme gruvbox-material]])
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
